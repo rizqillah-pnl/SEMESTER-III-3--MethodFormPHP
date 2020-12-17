b2e5dcc0f2acd7b0851470345d274b4217ce0263
Rizqillah <kazuryokun@gmail.com>
2020-12-17 17:35:11 +0700

    Menambah file baru

---
+++ Tugas/Get.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Method Get</title>
+</head>
+
+<body>
+  <form action="Get.php">
+    <label for="name">Username:</label><br>
+    <input type="text" id="name" name="name"><br><br>
+    <label for="password">Password:</label><br>
+    <input type="password" id="password" name="password"><br><br>
+    <input type="radio" id="male" name="gender" value="male">
+    <label for="male">Male</label><br>
+    <input type="radio" id="female" name="gender" value="female">
+    <label for="female">Female</label><br><br>
+    <input type="submit" value="Submit">
+    <input type="reset" value="Reset">
+  </form>
+
+
+
+
+
+  <?php
+  $nama = 'Rizqillah';
+  ?>
+
+
+</body>
+
+</html>
